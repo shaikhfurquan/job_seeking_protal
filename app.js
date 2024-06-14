786005920ae725c894548eed270f4dc050853f74
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-06-14 12:52:18 +0530

    cloudinary/file setup

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+import fileUpload from 'express-fileupload'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
-
 const app = express()
+
+
+// express middleware
+app.use(cors({
+    origin : [ process.env.FRONTEND_URL],
+    methods : ['GET' , 'POST' , 'PUT' , 'DELETE'],
+    credentials : true
+}))
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+// file upload setup
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : "/tmp/"
+}))
 
 export default app
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import app from './app.js'
+import cloudinary from 'cloudinary'
 
+
+// cloudinary config/setup
+cloudinary.v2.config({
+    cloud_name : process.env.CLOUDINARY_NAME ,
+    api_key : process.env.CLOUDINARY_API_KEY , 
+    api_secret : process.env.CLOUDINARY_SECRET ,
+})
 
 
 app.listen(process.env.PORT , ()=>{
